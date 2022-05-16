1e9a089d3b4395d8223fd50dc313247e52d3df54
mychmcg <mychmcg@github.com>
2022-05-16 08:47:53 -0400

    fixed neorg treesitter parser config

--- lua/ext/plugins.lua
+++ lua/ext/plugins.lua
@@ -1,5 +1,6 @@
 local fn = vim.fn
 
+
 -- Automatically install packer
 local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
@@ -30,24 +31,6 @@ packer.init {
   },
 }
 
--- These parsers add extra syntax highlighting to neorg
-local parser_configs = require('nvim-treesitter.parsers').get_parser_configs()
--- These two are optional and provide syntax highlighting
--- for Neorg tables and the @document.meta tag
-parser_configs.norg_meta = {
-    install_info = {
-        url = "https://github.com/nvim-neorg/tree-sitter-norg-meta",
-        files = { "src/parser.c" },
-        branch = "main"
-    },
-}
-parser_configs.norg_table = {
-    install_info = {
-        url = "https://github.com/nvim-neorg/tree-sitter-norg-table",
-        files = { "src/parser.c" },
-        branch = "main"
-    },
-}
 
 
 -- Plugin declarations
@@ -60,7 +43,6 @@ return packer.startup(function(use)
   use "lunarvim/colorschemes"
   use "rafamadriz/neon"
 
-  --
   -- plenary is a dependency of which-key.reset() & neorg, 
   use "nvim-lua/plenary.nvim"
   use { 
@@ -90,6 +72,27 @@ return packer.startup(function(use)
         require('neorg').setup {
           load = {
             ["core.defaults"] = {},
+            ["core.integrations.treesitter"] = {
+              config = {
+
+                norg      = {
+                  url = "https://github.com/nvim-neorg/tree-sitter-norg",
+                  files = { "src/parser.c", "src/scanner.cc" },
+                  branch = "main"
+                },
+                norg_meta = {
+                  url = "https://github.com/nvim-neorg/tree-sitter-norg-meta",
+                  files = { "src/parser.c" },
+                  branch = "main"
+                },
+                norg_table = {
+                  url = "https://github.com/nvim-neorg/tree-sitter-norg-table",
+                  files = { "src/parser.c" },
+                  branch = "main"
+                }
+              
+              }
+            },
             ["core.norg.concealer"] = {},
           }
         }
